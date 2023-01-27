405f3eb8298420d20c5c15b0e9e158245cee1bc7
she'll be <88987121+Romba89@users.noreply.github.com>
2023-01-27 17:39:31 +0100

    Add files via upload

--- slowdns.sh
+++ slowdns.sh
@@ -2,7 +2,7 @@
 clear
 #——————————————————
 # CREATED PER @VPSPLUS71
-# VERSION 1.3
+# VERSION 2.0
 # SLOW DNS TUNNEL
 #——————————————————
 RED='\033[1;31m'
@@ -10,7 +10,7 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 CYAN='\033[1;36m'
 CORTITLE='\033[1;41m'
-DIR='/etc/SSHPlus/dns'
+DIR='/etc/SLOWDNS/dns'
 SCOLOR='\033[0m'
 
 configdns() {
@@ -32,7 +32,7 @@ installslowdns() {
         sleep 2
         conexao
     }
-    mkdir /etc/SSHPlus/dns >/dev/null 2>&1
+    mkdir /etc/SLOWDNS/dns >/dev/null 2>&1
     wget -P $DIR https://raw.githubusercontent.com/Romba89/SLDNS/main/slowdns/dnstt-server >/dev/null 2>&1
     chmod 777 $DIR/dnstt-server >/dev/null 2>&1
     $DIR/dnstt-server -privkey-file $DIR/server.key -pubkey-file $DIR/server.pub >/dev/null 2>&1
@@ -51,7 +51,7 @@ initslow() {
             screen -r -S "slowdns" -X quit >/dev/null 2>&1
             screen -wipe >/dev/null 2>&1
             sed -i '/5300/d' /etc/autostart >/dev/null 2>&1
-            sed -i '/slowdns/d' /etc/SSHPlus/dns/autodns >/dev/null 2>&1
+            sed -i '/slowdns/d' /etc/SLOWDNS/dns/autodns >/dev/null 2>&1
             echo -e "\n${RED}SLOWDNS DESATIVADO !${SCOLOR}"
             sleep 2
             conexao
@@ -59,13 +59,13 @@ initslow() {
             screen -r -S "slowdns" -X quit >/dev/null 2>&1
             screen -wipe >/dev/null 2>&1
             sed -i '/5300/d' /etc/autostart >/dev/null 2>&1
-            rm -rf /etc/SSHPlus/dns >/dev/null 2>&1
+            rm -rf /etc/SLOWDNS/dns >/dev/null 2>&1
             echo -e "\n${RED}SLOWDNS REMOVED !${SCOLOR}"
             sleep 2
             conexao
         elif [[ "$op" == '3' ]]; then
             [[ -e $DIR/server.pub ]] && keypub=$(cat $DIR/server.pub) || keypub='Null'
-            [[ -e $DIR/autodns ]] && nameserver=$(grep -w 'server.key' /etc/SSHPlus/dns/autodns | awk -F' ' '{print $9}') || nameserver='Null'
+            [[ -e $DIR/autodns ]] && nameserver=$(grep -w 'server.key' /etc/SLOWDNS/dns/autodns | awk -F' ' '{print $9}') || nameserver='Null'
             tmx='curl -sO https://raw.githubusercontent.com/Romba89/slowdns/main/slowdns && chmod +x slowdns && ./slowdns'
             clear
             echo -e "${CORTITLE}           VPSPLUS SLOWDNS (Beta)            ${SCOLOR}"
@@ -155,9 +155,9 @@ echo -e "${RED}[${CYAN}5${RED}] ${YELLOW}SLOWDNS SHADOWSOCKS${SCOLOR}"
             configdns > /dev/null 2>&1
             iptables-save > /etc/iptables/rules.v4
         }
-        echo "screen -dmS slowdns $DIR/dnstt-server -udp :5300 -privkey-file $DIR/server.key ${ns} 0.0.0.0:${ptdns}" >/etc/SSHPlus/dns/autodns
-        chmod 777 /etc/SSHPlus/dns/autodns >/dev/null 2>&1
-        echo "ss -lu|grep -w '5300' || /etc/SSHPlus/dns/autodns" >>/etc/autostart
+        echo "screen -dmS slowdns $DIR/dnstt-server -udp :5300 -privkey-file $DIR/server.key ${ns} 0.0.0.0:${ptdns}" >/etc/SLOWDNS/dns/autodns
+        chmod 777 /etc/SLOWDNS/dns/autodns >/dev/null 2>&1
+        echo "ss -lu|grep -w '5300' || /etc/SLOWDNS/dns/autodns" >>/etc/autostart
         tmx='curl -sO https://raw.githubusercontent.com/Romba89/slowdns/main/slowdns && chmod +x slowdns && ./slowdns'
         echo -e "\n${GREEN}SLOWDNS ACTIV !${SCOLOR}"
         echo -e "\n${YELLOW}TERMUX COMMAND${SCOLOR}: ${tmx} ${ns} ${keypub}"
